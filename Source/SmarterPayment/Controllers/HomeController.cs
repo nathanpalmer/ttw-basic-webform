5ee119275eea639c8fd62a7a175643bb3bf629c2
nathanpalmer <nathan@nathanpalmer.com>
2011-09-01 22:28:22 -0400

    added template project

---
+++ Source/SmarterPayment/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+namespace SmarterPayment
+{
+	public class HomeController : Controller
+	{
+		public string Index()
+		{
+			return "Hello World";
+		}
+	}
+}
+
---
+++ Source/SmarterPayment/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Inherits="SmarterPayment.MvcApplication" %>
